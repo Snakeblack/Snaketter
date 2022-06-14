f5036da10d36c39462cedb0d365c85163965fdfc
sn4ke-laptop <michael.retamozo@gmail.com>
2022-06-14 20:37:30 +0200

    add: clone twitter started

---
+++ pages/timeline/index.js
@@ -0,0 +1,25 @@
+import style from "../../styles/Timeline.module.css";
+import Link from "next/link";
+
+const Timeline = ({ userName }) => {
+    return (
+        <>
+            <h1 className={style.h1}>This is the timeline of { userName }</h1>
+            <Link href="/">
+                <a className={style.gohome}>Go home</a>
+            </Link>
+        </>
+    )
+}
+
+export default Timeline;
+
+Timeline.getInitialProps = async () => {
+    return fetch('http://localhost:3000/api/hello')
+        .then(res => res.json())
+        .then(response => {
+            console.log(response)
+            const { userName } = response
+            return { userName }
+        })
+}
